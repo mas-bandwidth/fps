b0647c896500a7c849c27e167d3c3e515aeb7b7a
Glenn Fiedler <glenn@networknext.com>
2024-04-20 17:28:11 -0400

    implement xdp side of perf buffer

--- server.c
+++ server.c
@@ -144,7 +144,7 @@ int bpf_init( struct bpf_t * bpf, const char * interface_name )
 
     // create the input perf buffer
 
-    bpf->input_buffer = perf_buffer__new( bpf->input_buffer_fd, 250000, process_input, NULL, NULL, NULL );
+    bpf->input_buffer = perf_buffer__new( bpf->input_buffer_fd, 262144, process_input, NULL, NULL, NULL );
     if ( libbpf_get_error( bpf->input_buffer ) ) 
     {
         printf( "\nerror: could not create input buffer\n\n" );
