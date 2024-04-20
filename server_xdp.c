119e128ef82fad70f04af3770a712785503f72cd
Glenn Fiedler <glenn@networknext.com>
2024-04-20 08:03:03 -0400

    go

--- server_xdp.c
+++ server_xdp.c
@@ -168,7 +168,7 @@ SEC("server_xdp") int server_xdp_filter( struct xdp_md *ctx )
                                     struct session_data value;
                                     if ( bpf_map_update_elem( &session_map, &request->session_id, &value, BPF_NOEXIST ) == 0 )
                                     {
-                                        relay_printf( "created session 0x%llx", request->session_id );
+                                        debug_printf( "created session 0x%llx", request->session_id );
                                     }
 
                                     reflect_packet( data, sizeof(struct join_response_packet) );
