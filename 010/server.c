149e2eef6ddea8953e07018f806f4cebc2eaf14d
Glenn Fiedler <glenn@networknext.com>
2024-04-24 18:09:35 -0400

    go

--- 010/server.c
+++ 010/server.c
@@ -303,6 +303,21 @@ static void cleanup()
     fflush( stdout );
 }
 
+int pin_thread_to_cpu( int cpu ) 
+{
+   int num_cpus = sysconf(_SC_NPROCESSORS_ONLN );
+   if ( cpu < 0 || cpu >= num_cpus  )
+      return EINVAL;
+
+   cpu_set_t cpuset;
+   CPU_ZERO( &cpuset );
+   CPU_SET( cpu, &cpuset );
+
+   pthread_t current_thread = pthread_self();    
+
+   return pthread_setaffinity_np( current_thread, sizeof(cpu_set_t), &cpuset );
+}
+
 int main( int argc, char *argv[] )
 {
     signal( SIGINT,  interrupt_handler );
@@ -325,7 +340,7 @@ int main( int argc, char *argv[] )
 
     // main loop
 
-    pin_thread_to_core( MAX_THREADS );       // IMPORTANT: keep the main thread out of the way of the XDP cpus on google cloud [0,15]
+    pin_thread_to_cpu( MAX_THREADS );       // IMPORTANT: keep the main thread out of the way of the XDP cpus on google cloud [0,15]
 
     double last_print_time = platform_time();
 
