de97a874ccc0360f73998502240cfce8ab139358
Glenn Fiedler <glenn@networknext.com>
2024-04-24 18:05:49 -0400

    go

--- 010/shared.h
+++ 010/shared.h
@@ -28,6 +28,8 @@
 
 #define PLAYER_STATE_PACKET_SIZE                                        1 + PLAYER_STATE_SIZE
 
+#define HEAP_SIZE                                                                        2048
+
 #pragma pack(push, 1)
 
 struct join_request_packet
