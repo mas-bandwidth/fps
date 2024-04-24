e97ba42ac0bc298292be6b61f9b64e5f6a3b35b4
Glenn Fiedler <glenn@networknext.com>
2024-04-24 10:42:30 -0400

    go

--- 008/server_xdp.c
+++ 008/server_xdp.c
@@ -227,7 +227,7 @@ SEC("server_xdp") int server_xdp_filter( struct xdp_md *ctx )
 
                                     session_id = ( session_id ) % PLAYERS_PER_CPU;
 
-                                    __u8 * player_state = (__u8*) bpf_map_lookup_elem( &player_state_map, &session_id );
+                                    __u8 * player_state = (__u8*) bpf_map_lookup_elem( &cpu_player_state_map, &session_id );
                                     if ( !player_state )
                                     {
                                         debug_printf( "could not find player state for session %d", (int) session_id );
