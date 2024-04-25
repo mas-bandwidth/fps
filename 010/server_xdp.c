39d859618643a857a1cb2f3794ca8a9a1d0d856a
Glenn Fiedler <glenn@networknext.com>
2024-04-25 09:05:46 -0400

    go

--- 010/server_xdp.c
+++ 010/server_xdp.c
@@ -389,7 +389,7 @@ SEC("server_xdp") int server_xdp_filter( struct xdp_md *ctx )
 
                                     // respond with a player state packet for the client's local player
 
-                                    __u32 cpu = session_id % MAX_CPUS;
+                                    __u32 cpu = bpf_func_get_smp_processor_id();
 
                                     void * cpu_player_state_map = bpf_map_lookup_elem( &player_state_map, &cpu );
                                     if ( !cpu_player_state_map )
@@ -397,8 +397,6 @@ SEC("server_xdp") int server_xdp_filter( struct xdp_md *ctx )
                                         debug_printf( "could not find player state map for cpu %d", cpu );
                                         return XDP_DROP;
                                     }
-
-                                    session_id = ( session_id ) % PLAYERS_PER_CPU;
 
                                     __u8 * player_state = (__u8*) bpf_map_lookup_elem( cpu_player_state_map, &session_id );
                                     if ( !player_state )
