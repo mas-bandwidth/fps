48bfbacf971ab274e9a8b6ab47eb69d442e0f7cc
Glenn Fiedler <glenn@networknext.com>
2024-04-27 17:28:09 -0400

    go

--- 012/server_xdp.c
+++ 012/server_xdp.c
@@ -322,7 +322,7 @@ SEC("server_xdp") int server_xdp_filter( struct xdp_md *ctx )
 
                                         session->next_input_sequence = sequence + 1;
 
-                                        void * input_buffer = bpf_map_lookup_elem( input_buffer_map, &cpu );
+                                        void * input_buffer = bpf_map_lookup_elem( &input_buffer_map, &cpu );
                                         if ( !input_buffer )
                                         {
                                             debug_printf( "could not find input buffer for cpu %d", cpu );
