26197d4973f5c1f0f2e4caae01da5bce3fdbf545
Glenn Fiedler <glenn@networknext.com>
2024-04-30 10:27:15 -0400

    hacked up some code to pin golang code to cpu

---
+++ 016/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"runtime"
+	"strconv"
+)
+
+func main() {
+
+	if len(os.Args) != 2 {
+		fmt.Printf( "\nusage: go run server <cpu_index>\n\n")
+		os.Exit(0)
+	}
+
+	cpu, err :=	strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Printf( "\nerror: could not read cpu index\n\n")
+		os.Exit(1)
+	}
+
+	if runtime.GOOS == "linux" {
+		pid := os.Getpid()
+		cmd := exec.Command("taskset", "-c", fmt.Sprintf("%d", cpu), fmt.Sprintf("%d", pid))
+		if err := cmd.Run(); err != nil {
+			log.Fatal(err)
+		}	
+	}
+
+	runtime.GOMAXPROCS(1)
+
+	fmt.Printf("server running on cpu %d\n", cpu)
+
+	// todo: get ring buffer outer map
+
+	// todo: get ring buffer for cpu #
+
+	// todo: consume ring buffer
+}
