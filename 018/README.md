639a5a1587480ac7fb5ba143963e3d06114ff751
Glenn Fiedler <glenn@networknext.com>
2024-05-15 08:36:53 -0400

    add world database test for ping -> pong

--- 018/README.md
+++ 018/README.md
@@ -2,7 +2,7 @@
 
 Run with XDP ring buffer again, this time regularly yielding with runtime.Gosched() and making sure the go workers are actually pinned to the right CPU.
 
-Goal is to be able to handle 8k players per-bare metal machine with a 10G NIC.
+The goal is to demonstrate that it's possible to use goroutines running on a single CPU to process player inputs off the ring buffer, and to show that in this golang code it is also possible to block on TCP request/response calls while yielding to other player simulation updates.
 
 # Results
 
---
+++ 018/world_database.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+    "fmt"
+    "net"
+    "bufio"
+    "strings"
+)
+
+const Port = 50000
+
+func main() {
+
+    listener, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", Port))
+    if err != nil {
+        fmt.Printf("error: could not listen on tcp socket: %v\n", err)
+        return
+    }
+    defer listener.Close()
+
+    fmt.Printf("world database is listening on port %d\n", Port)
+
+    for {
+        conn, err := listener.Accept()
+        if err != nil {
+            fmt.Printf("error: could not accept client connection: %v\n", err)
+            continue
+        }
+        go handleConnection(conn)
+    }
+}
+
+func handleConnection(conn net.Conn) {
+
+    fmt.Printf("new connection from %s\n", conn.RemoteAddr().String())
+
+    defer conn.Close()
+
+    reader := bufio.NewReader(conn)
+
+    for {
+
+        line, err := reader.ReadString('\n')
+        if err != nil {
+            fmt.Printf("error: could not read line: %v\n", err)
+            return
+        }
+
+        line = strings.TrimSpace(string(line))
+
+        if line == "ping" {
+            fmt.Printf("%s ping -> pong\n", conn.RemoteAddr().String())            
+            conn.Write([]byte(string("pong\n")))
+        }
+
+        if line == "quit" {
+            fmt.Printf("%s quits\n", conn.RemoteAddr().String())
+            break
+        }
+
+    }
+}
